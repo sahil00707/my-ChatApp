715dec2c948816c06ade50148079d2494f7eb96c
unknown <ajmerisahil94@gmail.com>
2022-06-22 07:46:18 +0530

    Added index file and composer.json

--- index.php
+++ index.php
@@ -4,9 +4,15 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <title>Index</title>
 </head>
 <body>
-    
+    <?php  
+    echo "
+    <script>
+    window.location.href='register.php';
+    </script>
+    ";
+    ?>
 </body>
 </html>
