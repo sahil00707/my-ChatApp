99e83d3ce0d5bf770b5ea2fcf6d292efc94be1cb
unknown <ajmerisahil94@gmail.com>
2022-06-22 08:23:39 +0530

    Added index file and composer.json

--- common_links.php
+++ common_links.php
@@ -6,3 +6,6 @@
 <link href="https://fonts.googleapis.com/css2?family=Raleway:wght@300&display=swap" rel="stylesheet">
 
 <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+<link rel="preconnect" href="https://fonts.googleapis.com">
+<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+<link href="https://fonts.googleapis.com/css2?family=Nunito:wght@300&display=swap" rel="stylesheet">
